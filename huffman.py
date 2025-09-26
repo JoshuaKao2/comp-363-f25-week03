4dae0c5976ac64bef15ce93c7870f9e613402f1c
Joshua <119981783+JoshuaKao2@users.noreply.github.com>
2025-09-26 20:38:57 +0000

    finished

--- huffman.py
+++ huffman.py
@@ -53,13 +53,17 @@ def build_huffman_tree(frequencies: list[int]) -> Node:
     Builds the Huffman tree from the list of frequencies and returns the root Node.
     """
     forest = initialize_forest(frequencies)
-    # Your code here
+    #while there is more than one node
     while len(forest) > 1:
+        #get the 2 smallest nodes
         s1 = get_smallest(forest)
         s2 = get_smallest(forest)
+        #combines the 2 nodes
         new_node = Node(s1.get_frequency() + s2.get_frequency())
+        #sets the 2 nodes as children
         new_node.set_left(s1)
         new_node.set_right(s2)
+        #adds the new node back into the forest
         forest.append(new_node)
     return forest[0]
 
@@ -72,7 +76,6 @@ def get_smallest(forest):
     return forest.pop(smallest_index)
 
 
-
 def build_encoding_table(huffman_tree_root: Node) -> list[str]:
     """
     Builds the encoding table from the Huffman tree.
@@ -80,7 +83,27 @@ def build_encoding_table(huffman_tree_root: Node) -> list[str]:
     and index 26 corresponds to space.
     Each string is the binary encoding for that character.
     """
-    pass
+    
+    encoding_table = []
+    def traverse(node: Node, code: str):
+        # if is leaf node
+        if node.is_leaf():
+            char = node.get_char()
+            if char == " ":
+                encoding_table[26] = code
+            else:
+                index = ord(char) - ord("A")
+                encoding_table[index] = code
+            return
+
+        # if there are children, call again
+        if node.get_left():
+            traverse(node.get_left(), code + "0")
+        if node.get_right():
+            traverse(node.get_right(), code + "1")
+
+    traverse(huffman_tree_root, "")
+    return encoding_table
 
 
 def encode(input_string: str, encoding_table: list[str]) -> str:
@@ -89,13 +112,34 @@ def encode(input_string: str, encoding_table: list[str]) -> str:
     that the encoding table has 27 entries, one for each letter A-Z and
     one for space. Space is at the last index (26).
     """
-    pass
+    encoded_word = []
+    for char in input_string:    
+        if char == " ":
+            encoded_word.append(encoding_table[26])
+        else:
+            index = ord(char) - ord("A")
+            encoded_word.append(encoding_table[index])
+    return encoded_word
 
 
 def decode(encoded_string: str, huffman_root: Node) -> str:
     """
     Decodes the encoded string using the Huffman table as a key.
     """
-    pass
 
+    decoded_chars = []
+    current = huffman_root
 
+    for bit in encoded_string:
+        if bit == "0":
+            current = current.get_left()
+        else:
+            current = current.get_right()
+
+        # If we reach a leaf, add the letter
+        if current.get_left() is None and current.get_right() is None:
+            decoded_chars.append(current.get_char())
+            # resets for next character
+            current = huffman_root  
+
+    return decoded_chars
